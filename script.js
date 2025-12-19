f5c174a8f6f001912d7b911ca4322e5673309d34
Samuel Calles Garrido <samuelcallestorres@gmail.com>
2025-12-19 18:13:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const texts = [
+    "Desenvolupador Multiplataforma", 
+    "Especialista en PowerApps", 
+    "Suport IT & Helpdesk",
+    "Estudiant de DAM"
+];
+
+const textElement = document.getElementById('typewriter');
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentText = texts[textIndex];
+    
+    if (isDeleting) {
+        textElement.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        textElement.textContent = currentText.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    let typeSpeed = isDeleting ? 50 : 100;
+
+    if (!isDeleting && charIndex === currentText.length) {
+        typeSpeed = 2000; 
+        isDeleting = true;
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+        typeSpeed = 500;
+    }
+
+    setTimeout(type, typeSpeed);
+}
+
+document.addEventListener('DOMContentLoaded', type);
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
